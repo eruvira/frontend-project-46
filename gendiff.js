95f3c7eece47a377498c8cf66fc6407aebf9bb6e
Elvira Darisheva <d.elvira@rocketfirm.com>
2025-06-23 19:52:14 +0500

    feat: third step

--- gendiff.js
+++ gendiff.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 import { Command } from "commander";
+import parseFile from './src/parsers.js';
 
 const program = new Command();
 
@@ -10,10 +11,12 @@ program
   .version("0.0.1")
   .option("-f, --format [type]", "output format")
   .arguments("<filepath1> <filepath2>")
-  .action((filepath1, filepath2, options) => {
-    console.log("File 1:", filepath1);
-    console.log("File 2:", filepath2);
-    console.log("Format:", options.format);
+  .action((filepath1, filepath2) => {
+    const data1 = parseFile(filepath1);
+    const data2 = parseFile(filepath2);
+
+    console.log("File 1 parsed:", data1);
+    console.log("File 2 parsed:", data2);
   });
 
 program.parse();
