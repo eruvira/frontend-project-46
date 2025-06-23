ca38b6d383f8e2cd643c901171d77afed60cdfe1
Elvira Darisheva <d.elvira@rocketfirm.com>
2025-06-23 19:40:38 +0500

    feat: second step

--- gendiff.js
+++ gendiff.js
@@ -1,12 +1,19 @@
 #!/usr/bin/env node
 
-import { Command } from 'commander';
+import { Command } from "commander";
 
 const program = new Command();
 
 program
-  .name('gendiff')
-  .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1');
+  .name("gendiff")
+  .description("Compares two configuration files and shows a difference.")
+  .version("0.0.1")
+  .option("-f, --format [type]", "output format")
+  .arguments("<filepath1> <filepath2>")
+  .action((filepath1, filepath2, options) => {
+    console.log("File 1:", filepath1);
+    console.log("File 2:", filepath2);
+    console.log("Format:", options.format);
+  });
 
 program.parse();
