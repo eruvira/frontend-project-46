15af868fd45da7089c08348075f379cbb1bf8429
Elvira Darisheva <d.elvira@rocketfirm.com>
2025-06-24 15:03:40 +0500

    fix

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -9,7 +9,7 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1')
-  .option('-f, --format [type]', 'output format', 'plain')
+  .option('-f, --format [type]', 'output format', 'json')
   .arguments('<filepath1> <filepath2>')
  .action((filepath1, filepath2, options) => {
     const diff = genDiff(filepath1, filepath2, options.format)
